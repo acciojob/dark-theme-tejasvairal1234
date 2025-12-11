44657eeb0f470cbe8462a0f653267d7592ae01a8
tejasvairal1234 <tejasvairal7066@gmail.com>
2025-12-11 10:09:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
+function swapTheme() {
+  const appDiv = document.getElementById('app');
+  const swapBtn = document.getElementById('swap');
+  
+  if (appDiv.classList.contains('day')) {
+    appDiv.classList.remove('day');
+    appDiv.classList.add('night');
+    swapBtn.classList.remove('button_day');
+    swapBtn.classList.add('button_night');
+  } else {
+    appDiv.classList.remove('night');
+    appDiv.classList.add('day');
+    swapBtn.classList.remove('button_night');
+    swapBtn.classList.add('button_day');
+  }
+}
 
+const swapButton = document.getElementById('swap');
+swapButton.addEventListener('click', swapTheme);
